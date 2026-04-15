9a7b6a7d359d06d3902934e978906bcbe4f8f82e
Anahy <camila.beltrang24@cetis107.edu.mx>
2026-04-15 07:58:44 -0700

    BD Red Social BDR-P2-P1

---
+++ práctica 10/redes_sociales.sql
@@ -0,0 +1,312 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 15-04-2026 a las 16:57:51
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `redes_sociales`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comentarios`
+--
+
+CREATE TABLE `comentarios` (
+  `comentario_id` int(11) NOT NULL,
+  `pub_id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `texto` varchar(300) NOT NULL,
+  `fecha` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `comentarios`
+--
+
+INSERT INTO `comentarios` (`comentario_id`, `pub_id`, `usuario_id`, `texto`, `fecha`) VALUES
+(1, 1, 2, 'jeje', '2026-04-15 16:46:17'),
+(2, 4, 4, 'pepinos', '2026-04-15 16:46:17'),
+(3, 2, 1, 'ouyeaaa', '2026-04-15 16:46:17'),
+(4, 3, 5, 'PEPINO WAO', '2026-04-15 16:46:17'),
+(5, 5, 1, 'teamo', '2026-04-15 16:46:17');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `hashtags`
+--
+
+CREATE TABLE `hashtags` (
+  `hashtag_id` int(11) NOT NULL,
+  `etiqueta` varchar(60) NOT NULL,
+  `creado_en` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `hashtags`
+--
+
+INSERT INTO `hashtags` (`hashtag_id`, `etiqueta`, `creado_en`) VALUES
+(1, '#amoaminovia', '2025-06-02'),
+(2, '#hola', '2026-04-14'),
+(3, '#caballoazul', '2026-04-04'),
+(4, '#ausentes', '2025-12-13'),
+(5, '#pepinos', '2025-09-24');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `publicaciones`
+--
+
+CREATE TABLE `publicaciones` (
+  `pub_id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `contenido` text NOT NULL,
+  `imagen_url` varchar(255) NOT NULL,
+  `fecha_pub` datetime NOT NULL,
+  `likes` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `publicaciones`
+--
+
+INSERT INTO `publicaciones` (`pub_id`, `usuario_id`, `contenido`, `imagen_url`, `fecha_pub`, `likes`) VALUES
+(1, 1, 'amo a mi novia ', 'https://news.harvard.edu/gazette/story/2023/08/so-what-exactly-makes-taylor-swift-so-great/', '2026-04-15 16:32:58', 37),
+(2, 3, 'me gustan los caballos', 'https://www.nationalgeographic.com.es/animales/caballos', '2026-04-15 16:32:58', 38),
+(3, 4, 'pepinos', 'https://www.newscientist.com/article/mg24432601-700-alien-life-could-be-weirder-than-our-earthling-brains-can-ever-imagine/', '2026-04-15 16:36:25', 1000),
+(4, 5, 'en las calles de esta luna', 'https://www.instagram.com/ausentesband/', '2026-04-15 16:38:05', 300),
+(5, 2, '💞💞💞', 'https://www.billboard.com/lists/taylor-swift-the-fate-of-ophelia-su-no-1-mas-prolongado/', '2026-04-15 16:38:05', 49);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pub_hashtags`
+--
+
+CREATE TABLE `pub_hashtags` (
+  `pub_id` int(11) NOT NULL,
+  `hashtag_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `pub_hashtags`
+--
+
+INSERT INTO `pub_hashtags` (`pub_id`, `hashtag_id`) VALUES
+(1, 1),
+(5, 2),
+(4, 4),
+(2, 3),
+(3, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pub_likes`
+--
+
+CREATE TABLE `pub_likes` (
+  `pub_id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `fecha_like` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `pub_likes`
+--
+
+INSERT INTO `pub_likes` (`pub_id`, `usuario_id`, `fecha_like`) VALUES
+(1, 2, '2026-04-15 16:48:25'),
+(4, 3, '2026-04-15 16:48:25'),
+(2, 4, '2026-04-15 16:48:25'),
+(3, 5, '2026-04-15 16:48:25'),
+(5, 2, '2026-04-15 16:48:25');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `seguidores`
+--
+
+CREATE TABLE `seguidores` (
+  `seguidor_id` int(11) NOT NULL,
+  `seguido_id` int(11) NOT NULL,
+  `fecha_follow` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `seguidores`
+--
+
+INSERT INTO `seguidores` (`seguidor_id`, `seguido_id`, `fecha_follow`) VALUES
+(3, 1, '2025-07-23'),
+(1, 2, '2025-10-30'),
+(4, 5, '2025-07-22'),
+(5, 3, '2025-11-07'),
+(2, 4, '2026-01-15');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `usuario_id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `bio` varchar(200) NOT NULL,
+  `fecha_reg` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`usuario_id`, `username`, `email`, `bio`, `fecha_reg`) VALUES
+(1, '100piescayendo', '100piescayendo@cetis107.edu.mx', 'amo a mi novia', '2024-03-10'),
+(2, 'rubii._lr', 'rubi.lopez24@cetis107.edu.mx', 'hola', '2024-06-02'),
+(3, 'ddanioe', 'danna.carrillo24@cetis107.edu.mx', 'eee', '2025-03-18'),
+(4, 'ibbeaiw', 'abi.cerda24@cetis107.edu.mx', '🎇🎇🎇', '2022-12-13'),
+(5, 'rensssoe', 'renata.soto24@cetis107.edu.mx', 'como espiral en caracol', '2021-09-07');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD PRIMARY KEY (`comentario_id`),
+  ADD KEY `pub_id` (`pub_id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
+-- Indices de la tabla `hashtags`
+--
+ALTER TABLE `hashtags`
+  ADD PRIMARY KEY (`hashtag_id`);
+
+--
+-- Indices de la tabla `publicaciones`
+--
+ALTER TABLE `publicaciones`
+  ADD PRIMARY KEY (`pub_id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
+-- Indices de la tabla `pub_hashtags`
+--
+ALTER TABLE `pub_hashtags`
+  ADD KEY `pub_id` (`pub_id`),
+  ADD KEY `hashtag_id` (`hashtag_id`);
+
+--
+-- Indices de la tabla `pub_likes`
+--
+ALTER TABLE `pub_likes`
+  ADD KEY `pub_id` (`pub_id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
+-- Indices de la tabla `seguidores`
+--
+ALTER TABLE `seguidores`
+  ADD KEY `seguidor_id` (`seguidor_id`),
+  ADD KEY `seguido_id` (`seguido_id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`usuario_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  MODIFY `comentario_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `hashtags`
+--
+ALTER TABLE `hashtags`
+  MODIFY `hashtag_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `publicaciones`
+--
+ALTER TABLE `publicaciones`
+  MODIFY `pub_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `usuario_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD CONSTRAINT `comentarios_ibfk_1` FOREIGN KEY (`pub_id`) REFERENCES `publicaciones` (`pub_id`),
+  ADD CONSTRAINT `comentarios_ibfk_2` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`usuario_id`);
+
+--
+-- Filtros para la tabla `publicaciones`
+--
+ALTER TABLE `publicaciones`
+  ADD CONSTRAINT `publicaciones_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`usuario_id`);
+
+--
+-- Filtros para la tabla `pub_hashtags`
+--
+ALTER TABLE `pub_hashtags`
+  ADD CONSTRAINT `pub_hashtags_ibfk_1` FOREIGN KEY (`hashtag_id`) REFERENCES `hashtags` (`hashtag_id`),
+  ADD CONSTRAINT `pub_hashtags_ibfk_2` FOREIGN KEY (`pub_id`) REFERENCES `publicaciones` (`pub_id`);
+
+--
+-- Filtros para la tabla `pub_likes`
+--
+ALTER TABLE `pub_likes`
+  ADD CONSTRAINT `pub_likes_ibfk_1` FOREIGN KEY (`pub_id`) REFERENCES `publicaciones` (`pub_id`),
+  ADD CONSTRAINT `pub_likes_ibfk_2` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`usuario_id`);
+
+--
+-- Filtros para la tabla `seguidores`
+--
+ALTER TABLE `seguidores`
+  ADD CONSTRAINT `seguidores_ibfk_1` FOREIGN KEY (`seguidor_id`) REFERENCES `usuarios` (`usuario_id`),
+  ADD CONSTRAINT `seguidores_ibfk_2` FOREIGN KEY (`seguido_id`) REFERENCES `usuarios` (`usuario_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
